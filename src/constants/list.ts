ba7a833e18f7761e2a47022f6f78e748393db5a2
JoaoVictorNascimento <joao_victorxy@hotmail.com>
2026-03-23 17:10:09 -0300

    chore: create initial structure project

---
+++ src/constants/list.ts
@@ -0,0 +1,9 @@
+export const CARS_LIST = [
+    {name: 'Honda'},
+    {name: 'BMW'},
+    {name: 'Nissan'},
+    {name: 'Toyota'},
+    {name: 'BYD'},
+    {name: 'Jaguar'},
+    {name: 'Ford'},
+];
